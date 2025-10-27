5c89c529547462fabd866955c3b573de76fef3c4
jcvrabo <122891959+jcvrabo@users.noreply.github.com>
2025-10-27 17:23:49 +0100

    update git actions

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,5 +3,7 @@
 OUTPUT_DIR=$PWD/dist
 mkdir -p ${OUTPUT_DIR}
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $PWD/target/linux_amr64/id-broker -ldflags "-X github.com/rabobank/id-broker/cfg.Version=${VERSION} -X github.com/rabobank/id-broker/cfg.Commit=${COMMIT}"
-tar czf ${OUTPUT_DIR}/id-broker-linux-amd64-${VERSION}.tgz -C $PWD/target/linux_amr64 id-broker
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "${OUTPUT_DIR}"/idb-plugin-"${VERSION}"-linux-amd64 -ldflags "-X github.com/rabobank/idb-plugin/cfg.Version=${VERSION} -X github.com/rabobank/idb-plugin/cfg.Commit=${COMMIT}"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o "${OUTPUT_DIR}"/idb-plugin-"${VERSION}"-darwin-amd64 -ldflags "-X github.com/rabobank/idb-plugin/cfg.Version=${VERSION} -X github.com/rabobank/idb-plugin/cfg.Commit=${COMMIT}"
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o "${OUTPUT_DIR}"/idb-plugin-"${VERSION}"-darwin-arm64 -ldflags "-X github.com/rabobank/idb-plugin/cfg.Version=${VERSION} -X github.com/rabobank/idb-plugin/cfg.Commit=${COMMIT}"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o "${OUTPUT_DIR}"/idb-plugin-"${VERSION}"-window-amd64 -ldflags "-X github.com/rabobank/idb-plugin/cfg.Version=${VERSION} -X github.com/rabobank/idb-plugin/cfg.Commit=${COMMIT}"
